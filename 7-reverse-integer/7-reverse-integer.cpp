5da187eb1adae16fa87ea596b1d05a473468641b
HGY <62326152+guanying1110@users.noreply.github.com>
2022-07-04 00:22:48 +0800

    Time: 7 ms (19.26%), Space: 6.5 MB (6.59%) - LeetHub

---
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverse(int x) {
+        string num = to_string(abs(x));
+        ::reverse(num.begin(), num.end());
+        try {
+            return x >= 0 ? stoi(num) : -stoi(num);
+        }
+        catch(exception) {
+            return 0;
+        }
+    }
+};

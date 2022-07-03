d9059316ce31362412188a60479e121b01a7bf70
HGY <62326152+guanying1110@users.noreply.github.com>
2022-07-04 00:14:07 +0800

    Time: 56 ms (58.75%), Space: 89.8 MB (31.19%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -5,6 +5,6 @@ public:
         vector<int> save(all);
         merge(nums1.begin(), nums1.end(), nums2.begin(), nums2.end(), save.begin());
         
-        return all & 1 ? save[all / 2] : ((double)save[all / 2 - 1] + save[all / 2]) / 2;
+        return all & 1 ? save[all / 2] : (save[all / 2 - 1] + save[all / 2]) / 2.0;
     }
 };

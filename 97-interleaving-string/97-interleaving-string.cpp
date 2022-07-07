d28f2fc4c688816b672ec242a0a28ac509d99f7f
HGY <62326152+garyellow@users.noreply.github.com>
2022-07-07 14:49:30 +0800

    Time: 3 ms (85.12%), Space: 6.2 MB (89.89%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -3,21 +3,20 @@ public:
     bool isInterleave(string s1, string s2, string s3) {
         if(s3.size() != s1.size() + s2.size()) return false;
         
-        bool dp[s2.size() + 1];
-        memset(dp, 0, sizeof(dp));
-        dp[0] = true;
+        vector<bool> dp(s2.size() + 1);
+        dp.front() = true;
         
         for(int i = 0; i < s2.size(); i++)
             dp[i + 1] = dp[i] && s2[i] == s3[i];
         
         for(int i1 = 0; i1 < s1.size(); i1++)
         {
-            dp[0] = dp[0] && s1[i1] == s3[i1];
+            dp.front() = dp.front() && s1[i1] == s3[i1];
             
             for(int i2 = 0; i2 < s2.size(); i2++)
                 dp[i2 + 1] = ((dp[i2 + 1] && s1[i1] == s3[i1 + i2 + 1]) || (dp[i2] && s2[i2] == s3[i1 + i2 + 1]));
         }
         
-        return dp[s2.size()];
+        return dp.back();
     }
 };

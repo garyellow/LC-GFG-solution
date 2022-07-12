cb7ff9425cec3ae82b95fefe1ad3fa7f7e017242
HGY <gary20011110@gmail.com>
2022-07-12 14:40:34 +0800

    Time: 12 ms (98.93%), Space: 9.3 MB (89.17%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<bool> dp = vector<bool>(10101);
-    vector<bool> used = vector<bool>(201);
     
     bool canPartition(vector<int>& nums) {
         int sum = accumulate(nums.begin(), nums.end(), 0);
@@ -16,15 +15,10 @@ public:
             return true;
         else if(dp[now] == 0 && now < half)
         {
-            for(int i = index, len = nums.size(); i < len; i++)
+            for(int i = index + 1, len = nums.size(); i < len; i++)
             {
-                if (!used[i])
-                {
-                    used[i] = true;
-                    if(bt(nums, i, now + nums[i], half)) return true;
-                    else dp[now] = true;
-                    used[i] = false;
-                }
+                if(bt(nums, i, now + nums[i], half)) return true;
+                else dp[now] = true;
             }
         }
         return false;

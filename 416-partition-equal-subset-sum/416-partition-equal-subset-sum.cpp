766e05eab461ae92017d2ac13b9a1574736439d2
HGY <gary20011110@gmail.com>
2022-07-12 14:39:08 +0800

    Time: 8 ms (99.52%), Space: 9.3 MB (86.51%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,55 +1,32 @@
 class Solution {
 public:
-    vector<bool> dp = vector<bool>(20001, false);
-    vector<bool> used = vector<bool>(201, false);
-    int half;
-    bool check = false;
+    vector<bool> dp = vector<bool>(10101);
+    vector<bool> used = vector<bool>(201);
     
     bool canPartition(vector<int>& nums) {
-        int sum = 0;
-        for(auto i : nums)
-            sum += i;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
         
-        if(sum & 1)
-            return false;
-        else
-        {
-            half = sum / 2;
-            return bt(nums, 0, 0);
-        }
-        
+        if(sum & 1) return false;
+        else return bt(nums, 0, 0, sum / 2);
     }
     
-    bool bt(vector<int> nums, int now, int index)
+    bool bt(const vector<int> &nums, int index, int now, const int &half)
     {
-        if(check || now == half)
-        {
-            check = true;
+        if(now == half)
             return true;
-        }
-        else if(dp[now] || now > half)
-        {
-            dp[now] = true;
-            return false;
-        }
-        else
+        else if(dp[now] == 0 && now < half)
         {
             for(int i = index, len = nums.size(); i < len; i++)
             {
                 if (!used[i])
                 {
                     used[i] = true;
-                    bt(nums, now + nums[i], i);
+                    if(bt(nums, i, now + nums[i], half)) return true;
+                    else dp[now] = true;
                     used[i] = false;
                 }
-                
-                if(check)
-                    return true;
-                else
-                    dp[now] = true;
             }
-            
-            return false;
         }
+        return false;
     }
 };

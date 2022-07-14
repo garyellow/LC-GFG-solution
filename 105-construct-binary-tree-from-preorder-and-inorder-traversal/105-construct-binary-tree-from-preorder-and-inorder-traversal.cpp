87fc7778c804a40088c6526524399878cf63e85d
HGY <gary20011110@gmail.com>
2022-07-14 23:34:37 +0800

    Time: 86 ms (12.51%), Space: 74.4 MB (11.25%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -13,15 +13,15 @@ class Solution {
 public:
     unordered_map<int, TreeNode*> record;
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        for(auto &&i : preorder)
+            record[i] = new TreeNode(i);
+        
         return buildTree(vector<int>(preorder), vector<int>(inorder));
     }
     
     TreeNode* buildTree(vector<int>&& preorder, vector<int>&& inorder) {
-        if(record.empty())
-            for(auto &&i : preorder)
-                record[i] = new TreeNode(i);
-        
         if(inorder.empty()) return nullptr;
+        
         int n = find(inorder.begin(), inorder.end(), preorder.front()) - inorder.begin();
         record[preorder.front()]->left = buildTree(vector<int>(preorder.begin() + 1, preorder.begin() + n + 1), vector<int>(inorder.begin(), inorder.begin() + n));
         record[preorder.front()]->right = buildTree(vector<int>(preorder.begin() + n + 1, preorder.end()), vector<int>(inorder.begin() + n + 1, inorder.end()));

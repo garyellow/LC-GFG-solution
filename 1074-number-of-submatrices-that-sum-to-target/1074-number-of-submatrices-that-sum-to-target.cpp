87378fd31bd4417757f4aea79c358fe3720d5d8d
HGY <gary20011110@gmail.com>
2022-07-18 15:17:51 +0800

    Time: 1574 ms (22.62%), Space: 253.3 MB (27.19%) - LeetHub

--- 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
+++ 1074-number-of-submatrices-that-sum-to-target/1074-number-of-submatrices-that-sum-to-target.cpp
@@ -1,40 +1,28 @@
 class Solution {
 public:
     int numSubmatrixSumTarget(vector<vector<int>>& matrix, int target) {
-        for(int i = 1; i < matrix.size(); i++)
-            for(int j = 0; j < matrix.front().size(); j++)
-                matrix[i][j] += matrix[i - 1][j];
-                
-        for(int j = 1; j < matrix.front().size(); j++)
-            for(int i = 0; i < matrix.size() ; i++)
+        for(int i = 0; i < matrix.size(); i++)
+            for(int j = 1; j < matrix.front().size() ; j++)
                 matrix[i][j] += matrix[i][j - 1];
         
         int cnt = 0;
-        
-        for(int is = 0; is < matrix.size(); is++)
+        for(int js = 0; js < matrix.front().size(); js++)
         {
-            for(int ie = is; ie < matrix.size(); ie++)
+            for(int je = js; je < matrix.front().size(); je++)
             {
-                for(int js = 0; js < matrix.front().size(); js++)
+                int sum = 0;
+                unordered_map<int, int> dp;
+                dp[0] = 1;                
+                for(int i = 0; i < matrix.size(); i++)
                 {
-                    for(int je = js; je < matrix.front().size(); je++)
-                    {
-                        if(check(matrix, is, ie, js, je) == target)
-                            cnt++;
-                    }
+                    sum += matrix[i][je] - (js > 0 ? matrix[i][js - 1] : 0);
+                    cnt += dp[sum - target];
+                    dp[sum]++;
                 }
             }
         }
         
-        return cnt;
-    }
-    
-    int check(const vector<vector<int>>& matrix, int is, int ie, int js, int je)
-    {
-        int temp = matrix[ie][je] + (is - 1 >= 0 && js - 1 >= 0 ? matrix[is - 1][js - 1] : 0);
-        temp -= (is - 1 >= 0 ? matrix[is - 1][je] : 0);
-        temp -= (js - 1 >= 0 ? matrix[ie][js - 1] : 0);
         
-        return temp;
+        return cnt;
     }
 };

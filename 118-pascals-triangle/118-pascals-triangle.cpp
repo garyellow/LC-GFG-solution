d0c80ca35d17e55d6eba02bf0c142dc13dd87e9f
HGY <gary20011110@gmail.com>
2022-07-19 09:57:35 +0800

    Time: 0 ms (100.00%), Space: 6.6 MB (31.97%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    Solution()
+    {
+        if (dp.empty())
+        {
+            dp.push_back(vector<int>(1, 1));
+
+            for (int i = 1; i <= 30; i++)
+            {
+                vector<int> temp(1, 1);
+
+                for (int index = 1; index < dp.back().size(); index++)
+                    temp.push_back(dp.back()[index - 1] + dp.back()[index]);
+
+                temp.push_back(1);
+                dp.push_back(temp);
+            }
+        }
+    }
+
+    vector<vector<int>> generate(int numRows)
+    {
+        return {dp.begin(), dp.begin() + numRows};
+    }
+
+    static vector<vector<int>> dp;
+};
+
+vector<vector<int>> Solution::dp;

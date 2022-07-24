de4d87743a1e21f89602295a4ed30775033faaae
HGY <gary20011110@gmail.com>
2022-07-24 13:22:00 +0800

    Time: 235 ms (27.91%), Space: 14.8 MB (68.84%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        for(auto &&line : matrix)
+        {
+            if(line.front() <= target && line.back() >= target)
+                if(*lower_bound(line.begin(), line.end(), target) == target)
+                    return true;
+        }
+        
+        return false;
+    }
+};

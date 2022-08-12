099916c6012d55fbc9379de4467dce0798f2f7a0
HGY <gary20011110@gmail.com>
2022-08-12 22:45:10 +0800

    Time: 42 ms (63.71%), Space: 23.4 MB (12.34%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -13,17 +13,19 @@ public:
     vector<int> ans;
 
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root == p || root == q)
+        if(root->val == p->val || root->val == q->val || (root->val > p->val) == (root->val < q->val))
             return root;
         else
         {
-            TreeNode* P = root->left ? lowestCommonAncestor(root->left, p, q) : NULL;
-            TreeNode* Q = root->right ? lowestCommonAncestor(root->right, p, q) : NULL;
+            if(root->val > p->val) return lowestCommonAncestor(root->left, p, q);
+            else return lowestCommonAncestor(root->right, p, q);
+//             TreeNode* P = root->left ? lowestCommonAncestor(root->left, p, q) : NULL;
+//             TreeNode* Q = root->right ? lowestCommonAncestor(root->right, p, q) : NULL;
             
-            if(P && Q) return root;
-            else if(P) return P;
-            else if(Q) return Q;
-            else return NULL;
+//             if(P && Q) return root;
+//             else if(P) return P;
+//             else if(Q) return Q;
+//             else return NULL;
         }
     }
 };

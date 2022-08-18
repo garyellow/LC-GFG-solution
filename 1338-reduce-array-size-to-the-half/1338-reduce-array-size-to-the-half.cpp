8a6a681ffbac609112ca2813a9c636137e2d8bac
HGY <gary20011110@gmail.com>
2022-08-18 14:36:39 +0800

    Time: 497 ms (16.36%), Space: 80.2 MB (39.23%) - LeetHub

---
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+        map<int, int> cnt;
+        
+        for(auto &&i : arr)
+            cnt[i]++;
+        
+        priority_queue<int> heap;
+        
+        for(auto &&i : cnt)
+            heap.push(i.second);
+        
+        int half = arr.size() / 2;
+        
+        int ans;
+        for(ans = 0; half > 0; ans++)
+        {
+            half -= heap.top();
+            heap.pop();
+        }
+        
+        return ans;
+    }
+};

7a593d6be1737d30d03fd6869fbd0eb64b931f83
HGY <gary20011110@gmail.com>
2022-08-26 11:53:22 +0800

    Time: 5 ms (53.95%), Space: 7.3 MB (11.02%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    set<vector<int>> all_ans;
+    Solution() {
+        for(long i = 1; i <= INT_MAX; i *= 2)
+        {
+            vector<int> temp(10);
+            int cur = i;
+            
+            while(cur)
+            {
+                temp[cur % 10]++;
+                cur /= 10;
+            }
+            
+            all_ans.insert(temp);
+        }
+    }
+    bool reorderedPowerOf2(int n) {
+        vector<int> temp(10);
+        int cur = n;
+
+        while(cur)
+        {
+            temp[cur % 10]++;
+            cur /= 10;
+        }
+        
+        return all_ans.find(temp) != all_ans.end();
+    }
+};

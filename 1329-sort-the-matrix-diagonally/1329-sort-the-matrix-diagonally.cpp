fe30d38884d1872eebaedc62513c7f4a27077a64
HGY <gary20011110@gmail.com>
2022-08-28 23:51:51 +0800

    Time: 13 ms (77.01%), Space: 11 MB (5.01%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        vector<vector<int*>> temp;
-        
         for(int i = 1; i < mat.size(); i++)
         {
             vector<int*> cur;
@@ -10,7 +8,7 @@ public:
             for(int step = 0; step < mat.front().size() && i + step < mat.size(); step++)
                 cur.push_back(&(mat[i + step][step]));
             
-            temp.push_back(cur);
+            sort(cur);
         }
         
         vector<int*> cur;
@@ -18,7 +16,7 @@ public:
         for(int step = 0; step < mat.front().size() && step < mat.size(); step++)
             cur.push_back(&(mat[step][step]));
 
-        temp.push_back(cur);
+        sort(cur);
         
         for(int j = 1; j < mat.front().size(); j++)
         {
@@ -27,23 +25,23 @@ public:
             for(int step = 0; step < mat.size() && j + step < mat.front().size(); step++)
                 cur.push_back(&(mat[step][j + step]));
             
-            temp.push_back(cur);
-        }
-        
-        for(auto &&line : temp)
-        {
-            priority_queue<int, vector<int>, greater<int>> pq;
-            
-            for(auto &&i : line)
-                pq.push(*i);
-            
-            for(auto &&i : line)
-            {
-                *i = pq.top();
-                pq.pop();
-            }
+            sort(cur);
         }
         
         return mat;
+    }
+    
+    void sort(vector<int*> arr)
+    {
+        priority_queue<int, vector<int>, greater<int>> pq;
+            
+        for(auto &&i : arr)
+            pq.push(*i);
+
+        for(auto &&i : arr)
+        {
+            *i = pq.top();
+            pq.pop();
+        }
     }
 };

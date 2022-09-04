00c5c9aedb7c8a74d5579d4a5de00321c501b4b9
HGY <gary20011110@gmail.com>
2022-09-05 03:01:39 +0800

    Time: 10 ms (44.99%), Space: 12.9 MB (71.85%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,40 +11,29 @@
  */
 class Solution {
 public:
-    deque<vector<int>> ans = deque<vector<int>>(1,vector<int>());
-    int min = 0, max = 0, index = 0;
-    
+    map<int, vector<int>> record =  map<int, vector<int>>({{0, vector<int>()}});
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         DFS(root, 0, 0);
         
-        for(auto &&i : ans)
+        for(auto &&[cnt, arr] : record)
         {
-            sort(i.begin(), i.end(), [](auto &x, auto &y){ return (x % 10000 < y % 10000); });
-            stable_sort(i.begin(), i.end(), [](auto &x, auto &y){ return (x / 10000 < y / 10000); });
+            sort(arr.begin(), arr.end(), [](auto &x, auto &y){ return (x % 10000 < y % 10000); });
+            stable_sort(arr.begin(), arr.end(), [](auto &x, auto &y){ return (x / 10000 < y / 10000); });
             
-            for(auto &&j : i)
-                j %= 10000;
+            for(auto &&num : arr)
+                num %= 10000;
         }
             
-        return vector<vector<int>>(ans.begin(), ans.end());
+        vector<vector<int>> ans;
+        for(auto &&[cnt, arr] : record)
+            ans.push_back(arr);
+        
+        return ans;
     }
     
     void DFS(TreeNode* now, int x, int y)
     {
-        if(y < min)
-        {
-            ans.push_front(vector<int>());
-            index++;
-            min = y;
-        }
-        
-        if(y > max)
-        {
-            ans.push_back(vector<int>());
-            max = y;
-        }
-        
-        ans[index + y].push_back(now->val + 10000 * x);
+        record[y].push_back(now->val + 10000 * x);
         if(now->left) DFS(now->left, x + 1, y - 1);
         if(now->right) DFS(now->right, x + 1, y + 1);
     }

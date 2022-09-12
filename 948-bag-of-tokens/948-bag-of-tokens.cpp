da0dda63a9b44f7ab0c62c5943874ced177b3374
HGY <gary20011110@gmail.com>
2022-09-13 00:17:30 +0800

    Time: 19 ms (12.07%), Space: 10.9 MB (27.39%) - LeetHub

---
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int bagOfTokensScore(vector<int>& tokens, int power) {
+        deque<int> q(tokens.begin(), tokens.end());
+        
+        sort(q.begin(), q.end());
+        
+        int score = 0, ans = 0;
+        
+        while(q.size() && (power >= q.front() || score)) {
+            if(power >= q.front())
+            {
+                score++;
+                power -= q.front();
+                q.pop_front();
+            }
+            else
+            {
+                score--;
+                power += q.back();
+                q.pop_back();
+            }
+            
+            ans = max(ans, score);
+        }
+        
+        return ans;
+    }
+};

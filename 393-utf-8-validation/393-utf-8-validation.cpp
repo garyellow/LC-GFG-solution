3a6b5cad7429c6a13775be6ed00a04be04961b43
HGY <gary20011110@gmail.com>
2022-09-14 01:56:44 +0800

    Time: 34 ms (16.71%), Space: 14 MB (76.31%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.cpp
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -3,50 +3,31 @@ public:
     bool validUtf8(vector<int>& data) {
         reverse(data.begin(), data.end());
         
-        int cur = 0, num;
+        bool cur = false;
+        int num;
+        
         while(data.size()) {
             bitset<8> temp(data.back());
-            cout << cur << ' ' << num << ' ' << temp << endl;
             
             if(cur)
             {
-                if(num)
-                {
-                    num--;
+                if(num--)
                     if(temp[7] != 1 || temp[6] != 0)
                         return false;
-                }
                 
                 if(num == 0)
-                    cur = 0;
-                
-                data.pop_back();
+                    cur = false;
             }
-            else 
+            else if(temp[7])
             {
-                if(temp[7])
-                {
-                    if(temp[5] == 0)
-                    {
-                        cur = 1;
-                        num = 1;
-                    }
-                    else if(temp[4] == 0)
-                    {
-                        cur = 1;
-                        num = 2;
-                    }
-                    else if(temp[3] == 0)
-                    {
-                        cur = 1;
-                        num = 3;
-                    }
-                    else
-                        return false;
-                }
-                
-                data.pop_back();
+                cur = true;
+                if(temp[5] == 0) num = 1;
+                else if(temp[4] == 0) num = 2;
+                else if(temp[3] == 0) num = 3;
+                else return false;
             }
+            
+            data.pop_back();
         }
         
         return cur == 0;

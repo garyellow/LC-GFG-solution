26989245da95f82ff6e6080f62d4dfcb108dae13
HGY <gary20011110@gmail.com>
2022-09-19 13:51:56 +0800

    Time: 252 ms (17.94%), Space: 51.2 MB (12.59%) - LeetHub

---
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -0,0 +1,93 @@
+class Solution {
+public:
+    vector<vector<string>> findDuplicate(vector<string>& paths) {
+        map<string, vector<string>> record;
+        
+        for(auto &&path : paths)
+        {
+            string forder;
+            
+            for(auto &&ch : path)
+            {
+                if(ch == ' ') break;
+                forder += ch;
+            }
+            
+            path = path.substr(forder.size() + 1, path.size() - forder.size());
+            
+            // cout << forder << endl << path << endl;
+            // for(auto &&[name, content] : spilt(path))
+            //     cout << name << ' ' << content << endl;
+            
+            auto file = spilt(path);
+            
+            for(auto &&[name, content] : file)
+                record[content].push_back(forder + '/' + name);
+        }
+        
+        vector<vector<string>> ans;
+        
+        for(auto &&[content, path] : record)
+        {
+            if(path.size() <= 1) continue;
+            
+            vector<string> temp;
+            
+            for(auto &&each : path)
+                temp.push_back(each);
+            
+            ans.push_back(temp);
+        }
+            
+        
+        return ans;
+    }
+    
+    vector<pair<string, string>> spilt(string &files)
+    {
+        vector<pair<string, string>> all;
+        string file;
+        string file_name;
+        string file_content;
+            
+        for(auto &&ch : files)
+        {
+            if(ch == ' ')
+            {
+                for(auto &&c : file)
+                {
+                    if(c == '(')
+                    {
+                        file_content = file.substr(file_name.size() + 1, file.size() - file_name.size() - 2);
+                        break;
+                    }
+                    
+                    file_name += c;
+                }
+                
+                all.push_back({file_name, file_content});
+                file.clear();
+                file_name.clear();
+                file_content.clear();
+                continue;
+            }
+            
+            file += ch;
+        }
+        
+        for(auto &&c : file)
+        {
+            if(c == '(')
+            {
+                file_content = file.substr(file_name.size() + 1, file.size() - file_name.size() - 2);
+                break;
+            }
+
+            file_name += c;
+        }
+
+        all.push_back({file_name, file_content});
+        
+        return all;
+    }
+};

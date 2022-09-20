675d2d7a5a813bad080357dc94e201ae3735fb8b
HGY <gary20011110@gmail.com>
2022-09-20 22:32:42 +0800

    Time: 713 ms (21.45%), Space: 109.1 MB (54.50%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -3,14 +3,10 @@ public:
     int findLength(vector<int>& nums1, vector<int>& nums2) {
         vector<vector<int>> dp(nums1.size(), vector<int>(nums2.size()));
         
+        int cnt = 0;
         for(int i = 0; i < nums1.size(); i++)
             for(int j = 0; j < nums2.size(); j++)
-                dp[i][j] = (nums1[i] == nums2[j]) ? ((i == 0 || j == 0) ? 1 : dp[i - 1][j - 1] + 1) : 0;
-        
-        int cnt = 0;
-        for(auto &&i : dp)
-            for(auto &&j : i)
-                cnt = max(cnt, j);
+                cnt = max(cnt, dp[i][j] = (nums1[i] == nums2[j]) ? ((i == 0 || j == 0) ? 1 : dp[i - 1][j - 1] + 1) : 0);
         
         return cnt;
     }

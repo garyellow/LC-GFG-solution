99d0f12ac64e8ba3dd3289e30fe317a6f1b8b664
HGY <gary20011110@gmail.com>
2022-09-20 14:10:45 +0800

    Time: 687 ms (24.27%), Space: 108.9 MB (60.26%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -4,15 +4,8 @@ public:
         vector<vector<int>> dp(nums1.size(), vector<int>(nums2.size()));
         
         for(int i = 0; i < nums1.size(); i++)
-        {
             for(int j = 0; j < nums2.size(); j++)
-            {
-                if(nums1[i] == nums2[j])
-                    dp[i][j] = (i == 0 || j == 0) ? 1 : dp[i - 1][j - 1] + 1;
-                else
-                    dp[i][j] = 0;
-            }
-        }
+                dp[i][j] = (nums1[i] == nums2[j]) ? ((i == 0 || j == 0) ? 1 : dp[i - 1][j - 1] + 1) : 0;
         
         int cnt = 0;
         for(auto &&i : dp)

8845dfd83d249abec7a800d60135d99c075e9253
HGY <gary20011110@gmail.com>
2022-09-22 22:30:25 +0800

    Time: 48 ms (16.64%), Space: 11.1 MB (31.73%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<char> st;
+        
+        string ans;
+        for(auto &&ch : s)
+        {
+            if(ch == ' ')
+            {
+                while(st.size())
+                {
+                    ans.push_back(st.top());
+                    st.pop();
+                }
+                
+                ans.push_back(ch);
+            }
+            else
+                st.push(ch);
+        }
+        
+        while(st.size())
+        {
+            ans.push_back(st.top());
+            st.pop();
+        }
+        
+        return ans;
+    }
+};

c3cc8349449b11f3ce42975586365ac8f3d2d3c7
HGY <gary20011110@gmail.com>
2022-09-26 20:58:23 +0800

    Time: 7 ms (79.10%), Space: 11.7 MB (25.09%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     unordered_map<char, char> record;
     bool equationsPossible(vector<string>& equations) {
-        vector<bool> check(26);
         for(char i = 'a'; i <= 'z'; i++)
             record[i] = i;
         
@@ -13,18 +12,9 @@ public:
         for(auto&& equation : equations)
         {
             if(equation[1] == '=')
-            {
                 record[find(equation[0])] = find(equation[3]);
-                check[equation[0] - 'a'] = check[equation[3] - 'a'] = true;
-            }
-            else
-            {
-                if(equation[0] == equation[3] || check[equation[0] - 'a'] && check[equation[3] - 'a'])
-                {
-                    if(find(equation[0]) == find(equation[3]))
-                        return false;
-                }
-            }
+            else if(find(equation[0]) == find(equation[3]))
+                return false;
         }
         
         return true;

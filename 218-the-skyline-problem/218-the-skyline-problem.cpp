adc150a9c7cc7ac069be9adbee86868fa9f88894
HGY <gary20011110@gmail.com>
2022-10-01 00:46:26 +0800

    Time: 100 ms (17.57%), Space: 14.5 MB (82.66%) - LeetHub

---
+++ 218-the-skyline-problem/218-the-skyline-problem.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+        // stable_sort(buildings.begin(), buildings.end(), [](auto& x, auto &y){return x[2] > y[2];});
+        // stable_sort(buildings.begin(), buildings.end(), [](auto& x, auto &y){return x[1] < y[1];});
+        // stable_sort(buildings.begin(), buildings.end(), [](auto& x, auto &y){return x[0] < y[0];});
+        
+        for(auto &&building : buildings) {
+            swap(building[2], building[1]);
+            swap(building[1], building[0]);
+        }
+        
+        priority_queue<vector<int>> heap;
+        
+        int cur = 0, index = 0;
+        vector<vector<int>> ans;
+        while(heap.size() || index < buildings.size()) {
+            if(heap.empty())
+            {
+                int begin = buildings[index][1];
+                while(index < buildings.size() && buildings[index][1] == begin) heap.push(buildings[index++]);
+                
+                ans.push_back({heap.top()[1], heap.top()[0]});
+                cur = heap.top()[0];
+            }
+            else
+            {
+                if(index < buildings.size() && buildings[index][1] <= heap.top()[2])
+                {
+                    int begin = buildings[index][1];
+                    while(index < buildings.size() && buildings[index][1] == begin) heap.push(buildings[index++]);
+                    
+                    if(cur < heap.top()[0])
+                    {
+                        ans.push_back({heap.top()[1], heap.top()[0]});
+                        cur = heap.top()[0];
+                    }
+                }
+                else
+                {
+                    auto temp = heap.top()[2];
+                    while(heap.size() && heap.top()[2] <= temp)
+                        heap.pop();
+                    
+                    if(heap.empty())
+                    {
+                        ans.push_back({temp, 0});
+                        cur = 0;
+                    }
+                    else if(cur > heap.top()[0])
+                    {
+                        ans.push_back({temp, heap.top()[0]});
+                        cur = heap.top()[0];
+                    }
+                }
+            }
+        }
+        
+        return ans;
+    }
+};

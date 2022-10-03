6a7994f1aad2adca9b7b96fbd278f535871f30c3
HGY <gary20011110@gmail.com>
2022-10-04 00:31:19 +0800

    Time: 386 ms (20.51%), Space: 95.5 MB (19.54%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -3,20 +3,16 @@ public:
     int minCost(string colors, vector<int>& neededTime) {
         int cost = 0;
         
-        char last = 0;
         int sum = 0, max_cost = 0;
         for(int i = 0; i < colors.size(); i++) {
-            if(colors[i] == last) {
-                sum += neededTime[i];
-                max_cost = max(max_cost, neededTime[i]);
-            }
-            else {
+            if(i > 0 && colors[i] != colors[i - 1]) {
                 cost += sum - max_cost;
-                sum = neededTime[i];
-                max_cost = neededTime[i];
-                
-                last = colors[i];
+                sum = 0;
+                max_cost = 0;
             }
+            
+            sum += neededTime[i];
+            max_cost = max(max_cost, neededTime[i]);
         }
         
         return cost + sum - max_cost;

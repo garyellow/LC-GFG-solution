5e8694e3b812ffea25f3b6dd3dc170cee39373f4
HGY <gary20011110@gmail.com>
2022-10-10 13:22:45 +0800

    Time: 0 ms (100.00%), Space: 6.3 MB (5.00%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string breakPalindrome(string palindrome) {
+        if(palindrome.size() == 1) return "";
+        
+        for(int i = 0; i < palindrome.size(); i++) {
+            if(palindrome[i] != 'a' && (((palindrome.size() & 1) == 0) || (i != palindrome.size() / 2))) {
+                palindrome[i] = 'a';
+                return palindrome;
+            }
+        }
+        
+        palindrome.back() = 'b';
+        return palindrome;
+    }
+};

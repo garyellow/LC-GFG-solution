bae6da38a3ce971f0bb7ab39e510a8d2fd0bfbfe
HGY <gary20011110@gmail.com>
2022-10-19 14:10:00 +0800

    Time: 43 ms (5.81%), Space: 12.6 MB (56.15%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> record;
+        
+        for(auto &&word : words) {
+            record[word]++;
+        }
+        
+        auto cmp = [](pair<int, string> &x, pair<int, string>&y) {
+            return x.first == y.first ? x.second > y.second : x.first < y.first;
+        };
+        priority_queue<pair<int, string>, vector<pair<int, string>>, decltype(cmp)> heap(cmp);
+        
+        for(int i = 0; i < words.size(); i++) {
+            if(record[words[i]]) {
+                heap.push({record[words[i]], words[i]});
+                record[words[i]] = 0;
+            }
+        }
+        
+        vector<string> ans;
+        while(k--) {
+            ans.push_back(heap.top().second);
+            heap.pop();
+        }
+        
+        return ans;
+    }
+};

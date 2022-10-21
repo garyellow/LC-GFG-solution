a73a42ece76bad96573f527f5aaf4b636c1bff33
HGY <gary20011110@gmail.com>
2022-10-21 23:09:00 +0800

    Time: 417 ms (34.52%), Space: 77.2 MB (33.65%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> record;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            if(record.count(nums[i])) {
+                if(i - record[nums[i]] <= k) return true;
+            }
+            
+            record[nums[i]] = i;
+        }
+        
+        return false;
+    }
+};

c8d3ad295a8e63decd00eb2ea8d9b1aef061a555
HGY <gary20011110@gmail.com>
2022-11-11 00:53:42 +0800

    Time: 3 ms (80.87%), Space: 6.3 MB (74.12%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.cpp
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -1,15 +1,12 @@
 class Solution {
 public:
     string makeGood(string s) {
-        for(int i = 1; s.size() > 1 && i < s.size();) {
-            if(abs(s[i] - s[i - 1]) == 32) {
-                s.erase(s.begin() + i - 1);
-                s.erase(s.begin() + i - 1);
-                i = max(i - 1, 1);
-            }
-            else i++;
+        string str;
+        for(auto &&c : s) {
+            if(str.size() && abs(str.back() - c) == 32) str.pop_back();
+            else str.push_back(c);
         }
         
-        return s;
+        return str;
     }
 };

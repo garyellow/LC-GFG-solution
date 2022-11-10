0d0c373007b31f778db5b0a4f9b0a408ecdb9b58
HGY <gary20011110@gmail.com>
2022-11-11 00:42:51 +0800

    Time: 1240 ms (5.02%), Space: 9.8 MB (97.61%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        for(int i = 1; s.size() > 1 && i < s.size();) {
+            if(s[i] == s[i - 1]) {
+                s.erase(s.begin() + i - 1);
+                s.erase(s.begin() + i - 1);
+                i = max(i - 1, 1);
+            }
+            else i++;
+        }
+        
+        return s;
+    }
+};

93aad2a3a0d538145ec933ce7cd80190cfa10295
HGY <gary20011110@gmail.com>
2022-11-14 01:13:39 +0800

    Time: 36 ms (9.26%), Space: 47.5 MB (5.33%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        stringstream ss(s);
+        
+        vector<string> st;
+        
+        while(ss >> s) {
+            st.push_back(s + ' ');
+        }
+        
+        s = accumulate(st.rbegin(), st.rend(), string());
+        s.pop_back();
+        
+        return s;
+    }
+};

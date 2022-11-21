2afdc2a895ba6bf053a2c492bbc7003a11d16b9e
HGY <gary20011110@gmail.com>
2022-11-21 15:22:50 +0800

    Time: 405 ms (16.78%), Space: 33.5 MB (38.55%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
-        queue<vector<int>> bfs({{entrance[0] - 1, entrance[1]}, {entrance[0] + 1, entrance[1]}, {entrance[0], entrance[1] - 1}, {entrance[0], entrance[1] + 1}});
+        queue<pair<int, int>> bfs({{entrance[0] - 1, entrance[1]}, {entrance[0] + 1, entrance[1]}, {entrance[0], entrance[1] - 1}, {entrance[0], entrance[1] + 1}});
         maze[entrance[0]][entrance[1]] = '+';
         
         for(int step = 1; bfs.size(); step++)
@@ -11,18 +11,18 @@ public:
                 auto temp = bfs.front();
                 bfs.pop();  
                 
-                if(temp.front() < 0 || temp.back() < 0 || temp.front() >= maze.size() || temp.back() >= maze.front().size() || maze[temp[0]][temp[1]] == '+')
+                if(temp.first < 0 || temp.second < 0 || temp.first >= maze.size() || temp.second >= maze.front().size() || maze[temp.first][temp.second] == '+')
                     continue;
                 
-                if(temp.front() == 0 || temp.back() == 0 || temp.front() == maze.size() - 1 || temp.back() == maze.front().size() - 1)
+                if(temp.first == 0 || temp.second == 0 || temp.first == maze.size() - 1 || temp.second == maze.front().size() - 1)
                     return step;
                 else
                 {
-                    maze[temp[0]][temp[1]] = '+';
-                    bfs.push({temp.front() - 1, temp.back()});
-                    bfs.push({temp.front() + 1, temp.back()});
-                    bfs.push({temp.front(), temp.back() - 1});
-                    bfs.push({temp.front(), temp.back() + 1});
+                    maze[temp.first][temp.second] = '+';
+                    bfs.push({temp.first - 1, temp.second});
+                    bfs.push({temp.first + 1, temp.second});
+                    bfs.push({temp.first, temp.second - 1});
+                    bfs.push({temp.first, temp.second + 1});
                 }
             }
         }

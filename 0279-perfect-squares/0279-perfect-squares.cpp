35c0f4b0bb9ae608d6481ccd9dddf8b799ac3966
HGY <gary20011110@gmail.com>
2022-11-22 19:11:05 +0800

    Time: 463 ms (31.80%), Space: 272.1 MB (5.02%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> dp = vector<int>(1e5 + 1);
     int numSquares(int n) {
-        if(n == 0 || dp[n]) return dp[n];
+        if(dp[n] || n == 0) return dp[n];
             
         int temp = INT_MAX;
         for(int i = 1; n - i * i >= 0; i++)

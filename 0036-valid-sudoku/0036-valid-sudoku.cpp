72f9457c66adade6aff3775b978d5911d1042c8c
HGY <gary20011110@gmail.com>
2022-11-23 10:45:26 +0800

    Time: 43 ms (61.69%), Space: 22.3 MB (5.26%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -6,11 +6,15 @@ public:
         for(int i = 0; i < board.size(); i++) {
             for(int j = 0; j < board.front().size(); j++) {
                 if(board[i][j] == '.') continue;
+                
                 if(record[0][i].find(board[i][j]) == record[0][i].end()) record[0][i].insert(board[i][j]);
                 else return false;
+                
                 if(record[1][j].find(board[i][j]) == record[1][j].end()) record[1][j].insert(board[i][j]);
                 else return false;
-                if(record[2][i / 3 * 3 + j / 3].find(board[i][j]) == record[2][i / 3 * 3 + j / 3].end()) record[2][i / 3 * 3 + j / 3].insert(board[i][j]);
+                
+                int &&k = i / 3 * 3 + j / 3;
+                if(record[2][k].find(board[i][j]) == record[2][k].end()) record[2][k].insert(board[i][j]);
                 else return false;
             }
         }

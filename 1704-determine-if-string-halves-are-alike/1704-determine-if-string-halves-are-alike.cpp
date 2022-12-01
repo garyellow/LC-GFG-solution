eda65e4322089eed8c6cf86626853d538d0dc9e6
HGY <gary20011110@gmail.com>
2022-12-01 14:51:13 +0800

    Time: 3 ms (87.76%), Space: 6.7 MB (31.29%) - LeetHub

---
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        string vowels = "aeiouAEIOU";
+        auto cmp = [&vowels](char &val){
+            for(auto &&vowel : vowels)
+                if(val == vowel) return true;
+            
+            return false;
+        };
+        
+        return count_if(s.begin(), s.begin() + s.size() / 2, cmp) == count_if(s.begin() + s.size() / 2, s.end(), cmp);
+    }
+};

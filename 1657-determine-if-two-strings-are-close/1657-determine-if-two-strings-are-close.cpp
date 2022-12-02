ba5f6f78e4b73a68512b32578514ba3262a829df
HGY <gary20011110@gmail.com>
2022-12-03 02:17:56 +0800

    Time: 349 ms (18.56%), Space: 21.2 MB (5.39%) - LeetHub

---
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        unordered_map<char, int> hash1, hash2;
+        unordered_map<int, int> times1, times2;
+        unordered_set<int> char1, char2;
+        
+        for(auto &&c : word1)
+            hash1[c]++;
+        
+        for(auto &&c : word2)
+            hash2[c]++;
+        
+        for(auto &&[c, time] : hash1){
+            char1.insert(c);
+            times1[time]++;
+        }
+        
+        for(auto &&[c, time] : hash2){
+            char2.insert(c);
+            times2[time]++;
+        }
+        
+        return char1 == char2 && times1 == times2;
+    }
+};

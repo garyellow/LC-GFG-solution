29b80c9ee2d819612233875bd83daa21177cd01b
HGY <gary20011110@gmail.com>
2022-12-12 11:15:52 +0800

    Time: 3 ms (34.80%), Space: 6.1 MB (29.54%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        if(n == 2) return 2;
+        if(n <= 2) return n;
         
-        if(dp[n] == 0) dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
-        
-        return dp[n];
+        return dp[n] ? dp[n] : dp[n] = climbStairs(n - 1) + climbStairs(n - 2);
     }
     
     static vector<int> dp;

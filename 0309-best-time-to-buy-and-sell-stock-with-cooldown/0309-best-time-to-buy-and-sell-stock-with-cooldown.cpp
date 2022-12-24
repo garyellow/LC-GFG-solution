2f428a16ff591e5d98db68a40b718dac855f3c08
HGY <gary20011110@gmail.com>
2022-12-24 14:28:37 +0800

    Time: 9 ms (46.48%), Space: 12 MB (25.35%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -5,11 +5,11 @@ public:
         if(i >= prices.size()) return 0;
         else if(dp[i][turn] != -1) return dp[i][turn];
         else if(turn == 0)
-            return dp[i][turn] = max(-prices[i] + dfs(i + 1 , 1 , prices), dfs(i + 1 , 0 , prices));
+            return dp[i][turn] = max(-prices[i] + dfs(i + 1, 1, prices), dfs(i + 1, 0, prices));
         else if(turn == 1)
-            return dp[i][turn] = max(prices[i] + dfs(i + 1 , 2 , prices), dfs(i + 1 , 1 , prices));
+            return dp[i][turn] = max(prices[i] + dfs(i + 1, 2, prices), dfs(i + 1, 1, prices));
         else
-            return dp[i][turn] = dfs(i + 1 , 0 , prices);
+            return dp[i][turn] = dfs(i + 1, 0, prices);
     }
     
     int maxProfit(vector<int>& prices) {

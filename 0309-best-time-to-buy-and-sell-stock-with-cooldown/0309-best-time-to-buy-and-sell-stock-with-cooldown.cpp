d6803028ad441282661f4cc0fbdddff7dfebc4b4
HGY <gary20011110@gmail.com>
2022-12-24 14:26:57 +0800

    Time: 4 ms (80.62%), Space: 12.2 MB (15.40%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int dfs(int i , int turn , vector<int>& prices){
+    int dfs(int i , int turn , vector<int>& prices) {
         if(i >= prices.size()) return 0;
+        else if(dp[i][turn] != -1) return dp[i][turn];
         else if(turn == 0)
-            return dp[i][turn] == -1 ? dp[i][turn] = max(-prices[i] + dfs(i + 1 , 1 , prices), dfs(i + 1 , 0 , prices)) : dp[i][turn];
+            return dp[i][turn] = max(-prices[i] + dfs(i + 1 , 1 , prices), dfs(i + 1 , 0 , prices));
         else if(turn == 1)
-            return dp[i][turn] == -1 ? dp[i][turn] = max(prices[i] + dfs(i + 1 , 2 , prices), dfs(i + 1 , 1 , prices)) : dp[i][turn];
+            return dp[i][turn] = max(prices[i] + dfs(i + 1 , 2 , prices), dfs(i + 1 , 1 , prices));
         else
-            return dp[i][turn] == -1 ? dp[i][turn] = dfs(i + 1 , 0 , prices) : dp[i][turn];
+            return dp[i][turn] = dfs(i + 1 , 0 , prices);
     }
     
     int maxProfit(vector<int>& prices) {

184e8bbb8c7909b289a2caac679b30f74f167f2d
HGY <gary20011110@gmail.com>
2022-12-29 16:05:21 +0800

    Time: 678 ms (84.13%), Space: 129.3 MB (68.25%) - LeetHub

---
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        int time = 0;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+        
+        for(int i = 0; i < tasks.size(); i++)
+            tasks[i].push_back(i);
+        
+        sort(tasks.begin(), tasks.end());
+        
+        vector<int> ans;
+        for(int index = 0; index < tasks.size(); ) {
+            while(index < tasks.size() && time >= tasks[index].front()) {
+                heap.push({tasks[index][1], tasks[index][2]});
+                index++;
+            }
+            
+            if(heap.size()) {
+                auto [pt, id] = heap.top();
+                heap.pop();
+                
+                time += pt;
+                ans.push_back(id);
+            }
+            else {
+                time = tasks[index].front();
+            }
+        }
+        
+        while(heap.size()) {
+            ans.push_back(heap.top().second);
+            heap.pop();
+        }
+        
+        return ans;
+    }
+};

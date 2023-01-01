96ea7d3745bca044a5aecd35a086ca75ddc53752
HGY <gary20011110@gmail.com>
2023-01-01 14:43:29 +0800

    Time: 4 ms (32.33%), Space: 6.6 MB (19.87%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        vector<string> S = decode(s);
+        map<string, char> hash1;
+        map<char, string> hash2;
+        
+        if(S.size() != pattern.size()) return false;
+        
+        for(int i = 0; i < pattern.size(); i++)
+            if(hash1.count(S[i]) > 0 || hash2.count(pattern[i]) > 0) {
+                if(hash1[S[i]] != pattern[i] || hash2[pattern[i]] != S[i]) return false;
+            }
+            else {
+                hash1[S[i]] = pattern[i];
+                hash2[pattern[i]] = S[i];
+            }
+                
+        return true;
+    }
+    
+    vector<string> decode(string &ori) {
+        string temp;
+        vector<string> save;
+        for(auto &&i : ori)
+        {
+            if(isspace(i))
+            {
+                save.push_back(temp);
+                temp.clear();
+            }
+            else
+                temp.push_back(i);
+        }
+        
+        save.push_back(temp);
+        
+        return save;
+    }
+};

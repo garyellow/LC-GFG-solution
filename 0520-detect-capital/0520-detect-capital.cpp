e2755b7893083b029c15cbb66d409ad1ba763015
HGY <gary20011110@gmail.com>
2023-01-02 09:11:16 +0800

    Time: 5 ms (24.37%), Space: 6 MB (70.23%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word)
+    {
+        int lower = count_if(word.begin(), word.end(), [](int x){return islower(x);});
+
+        return lower == 0 || lower == word.size() || (lower == word.size() - 1 && isupper(word.front()));
+    }
+};

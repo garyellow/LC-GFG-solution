aa786252cff346f0a7c0d9494db62f3b9953a87f
HGY <gary20011110@gmail.com>
2023-01-04 15:17:48 +0800

    Time: 356 ms (27.75%), Space: 93.4 MB (53.77%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int cur_min = prices.front(), ans = 0;
+        
+        for(auto &&i : prices)
+            if(i >= cur_min)
+                ans = max(ans, i - cur_min);
+            else
+                cur_min = i;
+        
+        return ans;
+    }
+};

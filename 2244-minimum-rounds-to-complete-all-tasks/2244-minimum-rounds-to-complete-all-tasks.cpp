32ab74ff0d2de5f3e1fd3ff753d54f67f9e73231
HGY <gary20011110@gmail.com>
2023-01-04 15:13:41 +0800

    Time: 559 ms (32.85%), Space: 103.6 MB (72.00%) - LeetHub

---
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        unordered_map<int, int> hash;
+        
+        for(auto &&task : tasks) 
+            hash[task]++;
+        
+        int cnt = 0;
+        
+        for(auto &&[num, time] : hash)
+        {
+            if(time == 1)
+                return -1;
+            else {
+                cnt += time / 3;
+                cnt += (bool)(time % 3);
+            }
+        }
+        
+        return cnt;
+    }
+};

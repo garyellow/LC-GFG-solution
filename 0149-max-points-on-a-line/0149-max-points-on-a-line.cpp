b02c543d8d59e4de98e2a7132248a9ad7892077b
HGY <gary20011110@gmail.com>
2023-01-09 00:20:04 +0800

    Time: 42 ms (84.44%), Space: 20 MB (19.61%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,18 +1,23 @@
 class Solution {
 public:
     int maxPoints(vector<vector<int>>& points) {
+        sort(points.begin(), points.end());
+        
         int ans = 1;
         for(int i = 0; i < points.size(); i++) {
-            for(int j = i + 1; j < points.size(); j++) {
-                int cnt = 0;
-                double m = (double)(points[i][0] - points[j][0]) / (points[i][1] - points[j][1]);
+            unordered_map<double, int> record;
+            
+            for(int j = 0; j < points.size(); j++) {
+                double m;
+                if(i == j) continue;
+                else if(i > j) m = (double)(points[i][1] - points[j][1]) / (points[i][0] - points[j][0]);
+                else m = (double)(points[j][1] - points[i][1]) / (points[j][0] - points[i][0]);
                 
-                for(int k = 0; k < points.size(); k++) 
-                    if(i == k || (double)(points[i][0] - points[k][0]) / (points[i][1] - points[k][1]) == m)
-                        cnt++;
-                
-                ans = max(ans, cnt);
+                record[m]++;
             }
+            
+            if(record.size())
+                ans = max(ans, max_element(record.begin(), record.end(), [](auto &x, auto &y){return x.second < y.second;})->second + 1);
         }
         
         return ans;

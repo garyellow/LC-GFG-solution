ed937b647314e3eaa0c5471a4dd09111a75ebd4d
HGY <gary20011110@gmail.com>
2023-01-09 00:24:13 +0800

    Time: 60 ms (64.50%), Space: 19.8 MB (30.55%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -6,15 +6,15 @@ public:
             unordered_map<double, int> record;
             
             for(int j = 0; j < points.size(); j++) {
-                double m;
-                if(i == j) continue;
-                else m = (double)(points[i][1] - points[j][1]) / (points[i][0] - points[j][0]);
-                
-                record[m]++;
+                double m = 0;
+                if(i == j) record[m];
+                else {
+                    m = (double)(points[i][1] - points[j][1]) / (points[i][0] - points[j][0]);
+                    record[m]++;
+                }
             }
             
-            if(record.size())
-                ans = max(ans, max_element(record.begin(), record.end(), [](auto &&x, auto &&y){return x.second < y.second;})->second + 1);
+            ans = max(ans, max_element(record.begin(), record.end(), [](auto &&x, auto &&y){return x.second < y.second;})->second + 1);
         }
         
         return ans;

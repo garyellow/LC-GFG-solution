45d1e510826e3cb5f388cd63088859f1053c5ccf
HGY <gary20011110@gmail.com>
2023-01-21 13:40:06 +0800

    Time: 3 ms (65.69%), Space: 6.5 MB (89.32%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.cpp
@@ -11,17 +11,16 @@ public:
     void backtracking(int&& index, vector<string>& cur, string& s) {
         if(cur.size() > 4) return;
         if(cur.size() == 4 && index == s.size()) {
-            for(auto &&str : cur)
-                if(stoi(str) > 255 || str[0] == '0' && str.size() != 2)
-                    return;
-            
             auto &&temp = accumulate(cur.begin(), cur.end(), string());
             temp.pop_back();
             ans.push_back(temp);
         }
         
         for(int i = index; i < min(index + 4, int(s.size())); i++) {
-            cur.push_back(s.substr(index, i - index + 1) + ".");
+            auto &&temp = s.substr(index, i - index + 1);
+            if(stoi(temp) > 255 || temp[0] == '0' && temp.size() != 1) continue;
+            
+            cur.push_back(temp + ".");
             backtracking(i + 1, cur, s);
             cur.pop_back();
         }

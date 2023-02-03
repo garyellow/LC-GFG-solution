16842954c90b64f193f316e3c34741ead0c0fa10
HGY <gary20011110@gmail.com>
2023-02-03 14:42:30 +0800

    Time: 40 ms (29.12%), Space: 50.6 MB (9.31%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string convert(string& s, int numRows) {
+        if(numRows == 1) return s;
+        vector<string> datas(numRows);
+        
+        int index = 0;
+        bool zigzag = true; 
+        for(auto &&ch : s) {
+            if(zigzag) {
+                datas[index++].push_back(ch);
+                
+                if(index == numRows - 1) zigzag = !zigzag;
+            }
+            else {
+                datas[index--].push_back(ch);
+                
+                if(index == 0) zigzag = !zigzag;
+            }
+        }
+        
+        return accumulate(datas.begin(), datas.end(), string());
+    }
+}; 

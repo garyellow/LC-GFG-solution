773659747a6a3a21559979f0f84cadc461709df7
HGY <gary20011110@gmail.com>
2023-02-09 21:24:22 +0800

    Time: 2643 ms (5.69%), Space: 426.9 MB (5.69%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.cpp
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -3,7 +3,7 @@ public:
     long long distinctNames(vector<string>& ideas) {
         long long name = 0;
         vector<unordered_set<string>> arr(26);
-        for (string s : ideas) 
+        for (auto &&s : ideas) 
             arr[s[0] - 'a'].insert(s.substr(1));
         
         for (int i = 0; i < 25; i++) {

27101af56a10ccb906e867cd0d08d0d09cfcb37d
HGY <gary20011110@gmail.com>
2023-02-14 23:08:32 +0800

    Time: 0 ms (100.00%), Space: 6.3 MB (61.65%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string ans;
+        bool carry = false;
+        
+        while(a.size() || b.size())
+        {
+            int temp = carry;
+            if(a.size()) temp += a.back() - '0', a.pop_back();
+            if(b.size()) temp += b.back() - '0', b.pop_back();
+            
+            carry = temp / 2;
+            ans.push_back(temp % 2 + '0');
+        }
+        
+        if(carry) ans.push_back('1');
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};

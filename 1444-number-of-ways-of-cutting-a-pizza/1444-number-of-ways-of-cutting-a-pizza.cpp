c79f4ee23fe98f1da05c4faa3dbd245055e876fd
HGY <gary20011110@gmail.com>
2023-03-31 11:30:35 +0800

    Time: 108 ms (9.95%), Space: 10 MB (7.33%) - LeetHub

--- 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.cpp
+++ 1444-number-of-ways-of-cutting-a-pizza/1444-number-of-ways-of-cutting-a-pizza.cpp
@@ -1,56 +1,33 @@
 class Solution {
 public:
     unordered_map<int, int> dp;
+    vector<vector<int>> prefix;
     int ways(vector<string>& pizza, int k, int r = 0, int c = 0) {
-        int cnt = 0;
-        
-        for(int cr = r; cr < pizza.size(); cr++) {
-            for(int cc = c; cc < pizza.front().size(); cc++) {
-                if(pizza[cr][cc] == 'A') {
-                    cnt++;
+        if(prefix.empty()) {
+            prefix = vector<vector<int>>(pizza.size(), vector<int>(pizza.front().size()));
+            
+            for(int cr = pizza.size() - 1; cr >= 0; cr--)
+                for(int cc = pizza.front().size() - 1; cc >= 0; cc--) {
+                    if(pizza[cr][cc] == 'A') prefix[cr][cc]++;
+                    
+                    if(cr == pizza.size() - 1 && cc == pizza.front().size() - 1) continue;
+                    else if(cr == pizza.size() - 1) prefix[cr][cc] += prefix[cr][cc + 1];
+                    else if(cc == pizza.front().size() - 1) prefix[cr][cc] += prefix[cr + 1][cc];
+                    else prefix[cr][cc] += prefix[cr + 1][cc] + prefix[cr][cc + 1] - prefix[cr + 1][cc + 1];
                 }
-            }
         }
         
-        if(k == 1) return dp[encode(r, c, k)] = bool(cnt);
-        if(k > cnt) return dp[encode(r, c, k)] = false;
+        if(k == 1) return dp[encode(r, c, k)] = bool(prefix[r][c]);
+        if(k > prefix[r][c]) return dp[encode(r, c, k)] = false;
         
         if(!dp.count(encode(r, c, k))) {
-            for(int nr = r + 1; nr < pizza.size(); nr++) {
-                bool apple = false;
-                
-                for(int cr = r; cr < nr; cr++) {
-                    for(int cc = c; cc < pizza.front().size(); cc++) {
-                        if(pizza[cr][cc] == 'A') {
-                            apple = true;
-                            cr = nr;
-                            break;
-                        }
-                    }
-                }
-                
-                if(apple) {
+            for(int nr = r + 1; nr < pizza.size(); nr++) 
+                if(prefix[r][c] - prefix[nr][c]) 
                     dp[encode(r, c, k)] = (0L + dp[encode(r, c, k)] + ways(pizza, k - 1, nr, c)) % int(1e9 + 7);
-                }
-            }
             
-            for(int nc = c + 1; nc < pizza.front().size(); nc++) {
-                bool apple = false;
-                
-                for(int cr = r; cr < pizza.size(); cr++) {
-                    for(int cc = c; cc < nc; cc++) {
-                        if(pizza[cr][cc] == 'A') {
-                            apple = true;
-                            cr = pizza.size();
-                            break;
-                        }
-                    }
-                }
-                
-                if(apple) {
+            for(int nc = c + 1; nc < pizza.front().size(); nc++) 
+                if(prefix[r][c] - prefix[r][nc]) 
                     dp[encode(r, c, k)] = (0L + dp[encode(r, c, k)] + ways(pizza, k - 1, r, nc)) % int(1e9 + 7);
-                }
-            }
         }
         
         return dp[encode(r, c, k)];

22e29841ab3b3096e0e2f589e8baf973d1eeeedd
HGY <gary20011110@gmail.com>
2023-04-28 23:01:48 +0800

    Time: 44 ms (71.11%), Space: 10.5 MB (51.43%) - LeetHub

--- 0839-similar-string-groups/0839-similar-string-groups.cpp
+++ 0839-similar-string-groups/0839-similar-string-groups.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     int numSimilarGroups(vector<string>& strs) {
-        unordered_map<string, string> u;
-        for(auto &&str : strs)
-            u[str] = str;
+        unordered_map<int, int> u;
+        for(int i = 0; i < strs.size(); i++)
+            u[i] = i;
         
         for(int i = 0; i < strs.size(); i++) {
             for(int j = i + 1; j < strs.size(); j++) {
                 int cnt = 0;
-                for(int k = 0; k < strs.front().size(); k++)
+                for(int k = 0; k < strs.front().size() && cnt <= 2; k++)
                     cnt += (strs[i][k] != strs[j][k]);
                 
                 if(cnt == 0 || cnt == 2)
-                    u[find(strs[i], u)] = find(strs[j], u);
+                    u[find(i, u)] = find(j, u);
             }
         }
         
-        unordered_set<string> s;
+        unordered_set<int> s;
         for(auto &&[x, y] : u)
             s.insert(find(y, u));
         
         return s.size();
     }
     
-    string find(string val, unordered_map<string, string>& u) {
+    int find(int val, unordered_map<int, int>& u) {
         return u[val] == val ? val : u[val] = find(u[val], u);
     }
 };

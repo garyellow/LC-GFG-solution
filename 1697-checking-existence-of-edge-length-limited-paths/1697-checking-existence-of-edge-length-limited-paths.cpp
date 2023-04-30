dac338063f3486ebb4843183d8b7559aec6bd4b2
HGY <gary20011110@gmail.com>
2023-04-30 22:53:32 +0800

    Time: 619 ms (75.10%), Space: 109.5 MB (97.55%) - LeetHub

--- 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.cpp
+++ 1697-checking-existence-of-edge-length-limited-paths/1697-checking-existence-of-edge-length-limited-paths.cpp
@@ -1,21 +1,3 @@
-class UnionFind {
-    vector<int> parent;
-public:
-    UnionFind(int n) : parent(n) {
-        iota(parent.begin(), parent.end(), 0);
-    }
-    
-    void connect(int a, int b) {
-        int x = find(a), y = find(b);
-        if (x == y) return;
-        parent[x] = y;
-    }
-    bool connected(int i, int j) { return find(i) == find(j); }
-    int find(int a) {
-        return parent[a] == a ? a : (parent[a] = find(parent[a]));
-    }
-};
-
 class Solution {
 public:
     vector<bool> distanceLimitedPathsExist(int n, vector<vector<int>>& edgeList, vector<vector<int>>& queries) {
@@ -26,16 +8,18 @@ public:
         sort(queries.begin(), queries.end(), [](auto &a, auto &b) { return a[2] < b[2]; });
         sort(edgeList.begin(), edgeList.end(), [](auto &a, auto &b) { return a[2] < b[2]; });
         
-        UnionFind uf(n);
+        vector<int> u(n);
+        iota(u.begin(), u.end(), 0);
         
         int i = 0;
         for (auto &&q : queries) { 
             int x = q[0], y = q[1], limit = q[2], qid = q[3];
             for (; i < edgeList.size() && edgeList[i][2] < limit; i++) 
-                uf.connect(edgeList[i][0], edgeList[i][1]);
+                u[find(edgeList[i][0], u)] = find(edgeList[i][1], u);
             
-            ans[qid] = uf.connected(x, y);
+            ans[qid] = find(x, u) == find(y, u);
         }
+        
         return ans;
     }
     

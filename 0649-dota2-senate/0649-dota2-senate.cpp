34c38bccba69dbc118c320ad49eb3f21bd770940
HGY <gary20011110@gmail.com>
2023-05-05 01:43:33 +0800

    Time: 31 ms (14.12%), Space: 8.3 MB (11.07%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        int skip = 0;
+        
+        queue<char> s;
+        
+        for(auto &&p : senate)
+            s.push(p);
+        
+        while(abs(skip) < 1e4) {
+            auto i = s.front();
+            s.pop();
+            
+            if(skip == 0) {
+                if(i == 'R') skip++;
+                else skip--;
+                
+                s.push(i);
+            }
+            else if(skip > 0) {
+                if(i == 'R') {
+                    skip++;
+                    s.push(i);
+                }
+                else skip--;
+            }
+            else {
+                if(i == 'R')
+                    skip++;
+                else {
+                    skip--;
+                    s.push(i);
+                }
+            }
+        }
+        
+        return skip > 0 ? "Radiant" : "Dire";
+    }
+};

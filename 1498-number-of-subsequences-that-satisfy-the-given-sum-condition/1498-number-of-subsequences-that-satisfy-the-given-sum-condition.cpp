e72be240c16fd2837570704ac59b5dae2d7ebdc4
HGY <gary20011110@gmail.com>
2023-05-07 00:19:04 +0800

    Time: 144 ms (48.40%), Space: 48.6 MB (70.80%) - LeetHub

---
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    static vector<int> pow;
+    
+    Solution() {
+        if(pow.empty()) {
+            pow.push_back(1);
+            
+            for(int i = 1; i < 1e5; i++)
+                pow.push_back((pow.back() << 1) % int(1e9 + 7));
+        }
+    }
+    
+    int numSubseq(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        long ans = 0;
+        int l = 0;
+        int r = nums.size() - 1;
+        while (l <= r) {
+            if (nums[l] + nums[r] <= target) {
+                ans = int(ans + pow[r - l]) % int(1e9 + 7);
+                l++;
+            } 
+            else r--;
+        }
+        return ans % int(1e9 + 7);
+    }
+};
+
+vector<int> Solution::pow;

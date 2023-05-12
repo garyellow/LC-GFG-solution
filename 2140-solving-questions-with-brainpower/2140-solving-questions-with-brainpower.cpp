e9f6bb9ed7b63e4ccc3969d9f7ae248c47b7164a
HGY <gary20011110@gmail.com>
2023-05-13 01:46:09 +0800

    Time: 390 ms (37.08%), Space: 123 MB (44.72%) - LeetHub

---
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        vector<long long> dp(questions.size());
+        
+        return check(0, dp, questions);
+    }
+    
+    long long check(int question, vector<long long> &dp, vector<vector<int>>& questions) {
+        if(question >= questions.size()) return 0;
+        else if(dp[question]) {
+            return dp[question];
+        }
+        else {
+            return dp[question] = max(check(question + 1, dp, questions), questions[question][0] + check(question + questions[question][1] + 1, dp, questions));
+        }
+    }
+}; 

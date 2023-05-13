f708b6b1eea07afc4c973aece1735707769a8b70
HGY <gary20011110@gmail.com>
2023-05-13 17:32:35 +0800

    Time: 20 ms (42.12%), Space: 14.4 MB (37.54%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.cpp
@@ -4,19 +4,16 @@ public:
         vector<int> dp(high + 1, -1);
         dp.front() = 1;
         
-        long cnt = 0;
+        int cnt = 0;
         for(int i = low; i <= high; i++) 
             cnt = (cnt + check(i, dp, zero, one)) % int(1e9 + 7);
         
         return cnt;
     }
     
-    int check(int cur, vector<int> &dp, int zero, int one) {
+    long check(int cur, vector<int> &dp, int zero, int one) {
         if(cur < 0 || cur >= dp.size()) return 0;
-        else if(dp[cur] == -1) {
-            dp[cur] = check(cur - zero, dp, zero, one);
-            dp[cur] = (dp[cur] + long(check(cur - one, dp, zero, one))) % int(1e9 + 7);
-        }
+        else if(dp[cur] == -1) dp[cur] = (check(cur - zero, dp, zero, one) + check(cur - one, dp, zero, one)) % int(1e9 + 7);
         
         return dp[cur];
     }

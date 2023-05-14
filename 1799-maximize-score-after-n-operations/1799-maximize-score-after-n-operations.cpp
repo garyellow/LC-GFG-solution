5bec14ebd4deec20a58d4c438d7cb6a9108f9cbc
HGY <gary20011110@gmail.com>
2023-05-15 01:05:08 +0800

    Time: 95 ms (84.08%), Space: 13.9 MB (31.14%) - LeetHub

---
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxScore(vector<int>& nums) {
+        vector<int> dp(1 << 14, -1);
+
+        int m = nums.size(), n = nums.size()/2;
+        vector<vector<int>> g(nums.size(), vector<int>(nums.size()));
+        for(int i = 0; i < nums.size(); i++)
+            for(int j = i + 1; j < nums.size(); j++)
+                g[i][j] = gcd(nums[i], nums[j]);
+
+        return func(nums, 1, 0, dp, g);
+    }
+    
+    int func(vector<int>& nums, int op, int mask, vector<int>& dp, vector<vector<int>>& g){
+        int m = nums.size(), n = nums.size() / 2;
+        if(op > n) return 0;
+        if(dp[mask] != -1) return dp[mask];
+
+        for(int i = 0; i < m; i++){
+            if(mask & (1<<i)) continue;
+            for(int j = i + 1; j < m; j++){
+                if(mask & (1<<j)) continue;
+                
+                int newMask = (1<<i) | (1<<j) | mask;
+                int score = op * g[i][j] + func(nums, op + 1, newMask, dp, g);
+                dp[mask] = max(dp[mask], score);
+            }
+        }
+        return dp[mask];
+    }
+};

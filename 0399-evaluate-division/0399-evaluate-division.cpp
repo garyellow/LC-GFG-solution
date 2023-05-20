8e7c48e02952d09bc72582c71c2ba4673b6bb95a
garyellow <gary20011110@gmail.com>
2023-05-21 00:54:48 +0800

    Time: 0 ms (100.00%), Space: 8 MB (99.66%) - LeetHub

--- 0399-evaluate-division/0399-evaluate-division.cpp
+++ 0399-evaluate-division/0399-evaluate-division.cpp
@@ -3,21 +3,17 @@ public:
     unordered_map<string, string> u;
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
         unordered_map<string, double> record;
-        for(auto &&i : equations)
-        {
+        for(auto &&i : equations) {
             u[i.front()] = i.front();
             u[i.back()] = i.back();
         }
         
-        for(size_t i = 0; i < equations.size(); i++)
-        {
-            if(record.count(equations[i].front()) + record.count(equations[i].back()) == 0)
-            {
+        for(size_t i = 0; i < equations.size(); i++) {
+            if(record.count(equations[i].front()) + record.count(equations[i].back()) == 0) {
                 record[equations[i].front()] = values[i];
                 record[equations[i].back()] = 1;
             }
-            else if(record.count(equations[i].front()) + record.count(equations[i].back()) == 2)
-            {
+            else if(record.count(equations[i].front()) + record.count(equations[i].back()) == 2) {
                 double ratio = record[equations[i].front()] / values[i] / record[equations[i].back()];
                 for(auto &&r : record)
                     if(find(r.first) == find(equations[i].back()))
@@ -32,8 +28,7 @@ public:
         }
         
         vector<double> ans;
-        for(auto &&i : queries)
-        {
+        for(auto &&i : queries) {
             if(record.count(i.front()) && record.count(i.back()) && find(i.front()) == find(i.back()))
                 ans.push_back(record[i.front()] / record[i.back()]);
             else
@@ -43,8 +38,7 @@ public:
         return ans;
     }
     
-    string find(string val)
-    {
+    string find(string val) {
         return u[val] == val ? val : u[val] = find(u[val]);
     }
 };

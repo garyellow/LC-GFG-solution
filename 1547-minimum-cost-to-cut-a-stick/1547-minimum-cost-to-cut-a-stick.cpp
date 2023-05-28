bf7940b01d644fcea92bb514515538c13b07b9d8
garyellow <gary20011110@gmail.com>
2023-05-28 14:14:30 +0800

    Time: 184 ms (5.07%), Space: 10.3 MB (60.72%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
     int minCost(int n, vector<int>& cuts) {
-        unordered_map<int, unordered_map<int, int>> dp;
+        cuts.push_back(0);
+        cuts.push_back(n);
         
         sort(cuts.begin(), cuts.end());
         
-        return helper(0, n, cuts, dp);
+        vector<vector<int>> dp = vector<vector<int>>(cuts.size(), vector<int>(cuts.size(), INT_MAX));
+        
+        return helper(1, cuts.size() - 2, cuts, dp);
     }
     
-    int helper(int l, int r, vector<int>& cuts, unordered_map<int, unordered_map<int, int>> &dp) {
-        auto s = upper_bound(cuts.begin(), cuts.end(), l), e = lower_bound(cuts.begin(), cuts.end(), r);
-        
-        if(s == e) return 0;
-        else if(!dp[l][r]) {
-            dp[l][r] = INT_MAX;
-            for(auto it = s; it != e; it++) {
-                dp[l][r] = min(dp[l][r], helper(l, *it, cuts, dp) + helper(*it, r, cuts, dp) + r - l);
+    int helper(int l, int r, vector<int>& cuts, vector<vector<int>> &dp) {
+        if(l > r) return 0;
+        else if(dp[l][r] == INT_MAX) {
+            for(auto cur = l; cur <= r; cur++) {
+                dp[l][r] = min(dp[l][r], helper(l, cur - 1, cuts, dp) + helper(cur + 1, r, cuts, dp) + cuts[r + 1] - cuts[l - 1]);
             }
         }
         

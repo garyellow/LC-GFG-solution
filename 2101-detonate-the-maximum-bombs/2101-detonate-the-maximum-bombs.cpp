1627618dbd6eaf457ffe1ddd60f0be0b61002fda
garyellow <gary20011110@gmail.com>
2023-06-02 21:28:42 +0800

    Time: 797 ms (12.43%), Space: 14.8 MB (73.42%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int maximumDetonation(vector<vector<int>>& bombs) {
+        vector<vector<int>> g(bombs.size() + 1);
+        
+        for(int i = 0; i < bombs.size(); i++) {
+            for(int j = 0; j < bombs.size(); j++) {
+                if(i != j) {
+                    long x = pow(bombs[i][0] - bombs[j][0], 2);
+                    long y = pow(bombs[i][1] - bombs[j][1], 2);
+                    
+                    if(x + y <= 1L * bombs[i][2] * bombs[i][2]) {
+                        g[i].push_back(j);
+                    }
+                }
+            }
+        }
+    
+        int cnt = 0;
+        
+        for(int i = 0; i < bombs.size(); i++) {
+            vector<bool> isGone(bombs.size());
+            isGone[i] = true;
+            cnt = max(cnt, detonate(i, isGone, g));
+        }
+        
+        return cnt;
+    }
+    
+    int detonate(int cur, vector<bool> &isGone, vector<vector<int>> &g) {
+        int cnt = 1;
+        
+        for(auto &&next : g[cur]) {
+            if(!isGone[next]) {
+                cout << cur << ' ' << next << endl;
+                isGone[next] = true;
+                cnt += detonate(next, isGone, g);
+            }
+        }
+        
+        return cnt;
+    }
+};

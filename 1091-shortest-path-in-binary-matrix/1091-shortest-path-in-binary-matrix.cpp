a587659a8949846e6f8bc474e2fce38993bd741f
garyellow <gary20011110@gmail.com>
2023-06-02 16:58:05 +0800

    Time: 63 ms (91.25%), Space: 19.3 MB (85.37%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        if(grid.front().front()) return -1;
+        if(grid.front().front() || grid.back().back()) return -1;
         
-        vector<vector<bool>> isGone = vector<vector<bool>>(grid.size(), vector<bool>(grid.size()));
-        
-        isGone.front().front() = true;
+        grid.front().front() = true;
         queue<pair<int, int>> bfs({{0, 0}});
         
         for(int step = 2; bfs.size(); step++) {
@@ -15,8 +13,8 @@ public:
                 
                 for(int dx = -1; dx <= 1; dx++) {
                     for(int dy = -1; dy <= 1; dy++) {
-                        if(x + dx >= 0 && x + dx < grid.size() && y + dy >= 0 && y + dy < grid.size() && !grid[x + dx][y + dy] && !isGone[x + dx][y + dy]) {
-                            isGone[x + dx][y + dy] = true;
+                        if(x + dx >= 0 && x + dx < grid.size() && y + dy >= 0 && y + dy < grid.size() && !grid[x + dx][y + dy]) {
+                            grid[x + dx][y + dy] = true;
                             if(x + dx == grid.size() - 1 && y + dy == grid.size() - 1) return step;
                             else bfs.push({x + dx, y + dy});
                         }

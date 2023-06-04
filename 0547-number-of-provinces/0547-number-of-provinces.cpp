3dc19d72c70cac9a9710d6d2c603206f4323f289
garyellow <gary20011110@gmail.com>
2023-06-04 12:42:24 +0800

    Time: 21 ms (89.13%), Space: 13.8 MB (71.32%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        vector<int> u(isConnected.size());
+        iota(u.begin(), u.end(), 0);
+        
+        
+        for(int i = 0; i < isConnected.size(); i++) {
+            for(int j = 0; j < isConnected.size(); j++) {
+                if(isConnected[i][j]) {
+                    u[find(i, u)] = find(j, u);
+                }
+            }
+        }
+        
+        unordered_set<int> s;
+        
+        for(auto &&p : u) 
+            s.insert(find(p, u));
+        
+        return s.size();
+    }
+    
+    int find(int val, vector<int> &u) {
+        return val == u[val] ? val : u[val] = find(u[val], u);
+    }
+};

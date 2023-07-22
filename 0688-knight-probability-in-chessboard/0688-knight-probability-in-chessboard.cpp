b65d953ce4e3e322fc125a347f983e853ab8427b
garyellow <gary20011110@gmail.com>
2023-07-22 16:53:46 +0800

    Time: 33 ms (31.21%), Space: 8.8 MB (59.98%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -4,15 +4,13 @@ public:
     static vector<pair<int, int>> next;
     double knightProbability(int n, int k, int row, int column) {
         if(dp.empty()) {
-            dp = vector<vector<vector<double>>>(n, vector<vector<double>>(n, vector<double>(k + 1, -1)));
+            dp = vector<vector<vector<double>>>(n, vector<vector<double>>(n, vector<double>(k + 1)));
         }
         
         if(row < 0 || column < 0 || row >= n || column >= n) return 0;
         if(k == 0) return row >= 0 && column >= 0 && row < n && column < n;
         
-        if(dp[row][column][k] == -1) {
-            dp[row][column][k] = 0;
-            
+        if(!dp[row][column][k]) {
             for(auto &&[dx, dy]: next) {
                 dp[row][column][k] += 1.0 / 8 * knightProbability(n, k - 1, row + dx, column + dy);
             }

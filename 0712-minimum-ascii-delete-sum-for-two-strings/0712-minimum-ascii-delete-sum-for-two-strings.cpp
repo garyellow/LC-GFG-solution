bbd8d8065d201767e33376dc0da1b89ddec73d16
garyellow <gary20011110@gmail.com>
2023-07-31 22:47:24 +0800

    Time: 64 ms (41.47%), Space: 15.2 MB (68.25%) - LeetHub

---
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimumDeleteSum(string s1, string s2) {
+        vector<vector<int>> dp(s1.size(), vector<int>(s2.size(), INT_MAX));
+        
+        return helper(0, 0, s1, s2, dp);
+    }
+    
+    int helper(int i, int j, string &s1, string &s2, vector<vector<int>> &dp) {
+        if(i == s1.size()) {
+            return accumulate(s2.begin() + j, s2.end(), 0);
+        }
+        if(j == s2.size()) {
+            return accumulate(s1.begin() + i, s1.end(), 0);
+        }
+        
+        if(dp[i][j] == INT_MAX) {
+            int temp = INT_MAX;
+            
+            temp = min(temp, s1[i] + helper(i + 1, j, s1, s2, dp));
+            temp = min(temp, s2[j] + helper(i, j + 1, s1, s2, dp));
+            if(s1[i] == s2[j]) temp = min(temp, helper(i + 1, j + 1, s1, s2, dp));
+            
+            dp[i][j] = temp;
+        }
+        
+        return dp[i][j];
+    }
+};

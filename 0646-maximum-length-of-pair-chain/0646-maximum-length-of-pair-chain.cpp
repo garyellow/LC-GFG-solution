be64d8b03dee65179e0699c3c13e51ff915a6944
HGY <gary20011110@gmail.com>
2023-08-26 23:47:48 +0800

    Time: 46 ms (97.3%), Space: 22.4 MB (86.44%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.cpp
@@ -5,15 +5,14 @@ public:
             return x.back() < y.back();
         });
 
-        vector<int> dp(2002);
-        for(int i = 1, index = 0; i <= 2001; i++) {
-            dp[i] = dp[i - 1];
-
-            for(; index < pairs.size() && pairs[index].back() + 1 == i - 1000; index++) {
-                dp[i] = max(dp[i], dp[pairs[index].front() + 1000] + 1);
+        int cnt = 0;
+        for(int i = 0, index = 0; i < pairs.size(); i++) {
+            if(pairs[i].front() > pairs[index].back()) {
+                index = i;
+                cnt++;
             }
         }
 
-        return dp.back();
+        return cnt + 1;
     }
 };

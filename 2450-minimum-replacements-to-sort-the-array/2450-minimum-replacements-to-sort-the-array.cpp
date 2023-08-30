1e382738f72526fdac4409d9c63a44020720d7ea
HGY <gary20011110@gmail.com>
2023-08-30 22:41:56 +0800

    Time: 66 ms (99.7%), Space: 54.7 MB (7.81%) - LeetHub

--- 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.cpp
+++ 2450-minimum-replacements-to-sort-the-array/2450-minimum-replacements-to-sort-the-array.cpp
@@ -4,7 +4,6 @@ public:
         int cur = nums.back();
         long long ans = 0;
         for(int i = nums.size() - 2; i >= 0; i--) {
-            cout << cur << endl;
             int temp = nums[i];
             if(temp > cur) {
                 int cnt = ceil(1.0 * temp / cur);

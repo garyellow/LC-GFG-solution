9910c83b2cbac5408b68bd99129e3979704c8e89
HGY <gary20011110@gmail.com>
2023-09-24 10:02:39 +0800

    Time: 9 ms (47.64%), Space: 6.3 MB (76.54%) - LeetHub

---
+++ 0815-champagne-tower/0815-champagne-tower.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    double champagneTower(int poured, int query_row, int query_glass)
+    {
+        array<array<double, 101>, 101> dp({(double)poured});
+
+        for (size_t i = 0; i < 100; i++)
+            for (size_t j = 0; j <= i; j++)
+                if (dp[i][j] > 1) {
+                    dp[i + 1][j] += (dp[i][j] - 1) / 2;
+                    dp[i + 1][j + 1] += (dp[i][j] - 1) / 2;
+                    dp[i][j] = 1;
+                }
+
+        return dp[query_row][query_glass];
+    }
+};

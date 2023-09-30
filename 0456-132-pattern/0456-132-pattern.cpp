552204d60023cfcd7aa8377ed3f883aaaa6c0c11
HGY <gary20011110@gmail.com>
2023-09-30 22:21:50 +0800

    Time: 73 ms (33.8%), Space: 49.2 MB (27.66%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
         stack<int> s;
-        int mid = INT_MIN;
+        int k = INT_MIN;
 
         for (int i = nums.size() - 1; i >= 0; i--) {
-            if (nums[i] < mid) return true;
+            if (nums[i] < k) return true;
             while (!s.empty() && s.top() < nums[i]) {
-                mid = s.top();
+                k = s.top();
                 s.pop();
             }
             s.push(nums[i]);

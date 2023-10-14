5eacbaabf17926ddc53bde8fe42179dee63f54ed
HGY <gary20011110@gmail.com>
2023-10-14 20:00:06 +0800

    Time: 80 ms (97.35%), Space: 89.9 MB (85.65%) - LeetHub

--- 2808-painting-the-walls/2808-painting-the-walls.cpp
+++ 2808-painting-the-walls/2808-painting-the-walls.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int paintWalls(vector<int>& cost, vector<int>& time) {
-        vector<vector<int>> dp(501, vector<int>(501, -1));
-        return dfs(0, cost.size(), cost, time, dp);
+        vector<int> dp(cost.size() + 1, 1e9);
+        dp[0] = 0;
+
+        for (int i = 0; i < cost.size(); ++i) {
+            for (int j = cost.size(); j > 0; --j) {
+                dp[j] = std::min(dp[j], dp[std::max(j - time[i] - 1, 0)] + cost[i]);
+            }
+        }
+        
+        return dp[cost.size()];
     }
 
-    int dfs(int cur, int total, vector<int>& cost, vector<int>& time, vector<vector<int>> &dp) {
-        if(total <= 0) return 0;
-        if(cur == cost.size()) return 1e9;
-        if(dp[cur][total] != -1) return dp[cur][total];
-
-        int free = dfs(cur + 1, total, cost, time, dp);
-        int paid = cost[cur] + dfs(cur + 1, total - time[cur] - 1, cost, time, dp);
-        return dp[cur][total] = min(free, paid);
-    }
 };

aefeae7980b2a37bfa56616d619c78d56130eedb
garyellow <gary20011110@gmail.com>
2023-10-23 00:10:53 +0800

    Time: 128 ms (80.26%), Space: 89.8 MB (73.03%) - LeetHub

---
+++ 1918-maximum-score-of-a-good-subarray/1918-maximum-score-of-a-good-subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int maximumScore(vector<int>& nums, int k) {
+        for(int i = k, m = nums[k]; i >= 0; i--) {
+            m = nums[i] = min(m, nums[i]);
+        }
+        for(int i = k, m = nums[k]; i < nums.size(); i++) {
+            m = nums[i] = min(m, nums[i]);
+        }
+
+        int ans = nums.size() * min(nums.front(), nums.back());
+        for(int i = k, j = k; i != 0 || j != nums.size() - 1; ) {
+            if(i == 0) j++;
+            else if(j == nums.size() - 1) i--;
+            else if(nums[i - 1] > nums[j + 1]) i--;
+            else if(nums[i - 1] < nums[j + 1]) j++;
+            else i--, j++;
+
+            ans = max(ans, (j - i + 1) * min(nums[i], nums[j]));
+        }
+
+        return ans;
+    }
+};

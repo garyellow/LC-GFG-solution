c594110ef2b9f77f16c198a40e2a828bf1128c9c
garyellow <gary20011110@gmail.com>
2025-06-02 17:34:38 +0800

    Time: 12 ms (9.04%), Space: 23.4 MB (22.48%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -5,12 +5,16 @@ public:
         vector<int> index(ratings.size());
         iota(index.begin(), index.end(), 0);
 
-        sort(index.begin(), index.end(), [&](auto &i, auto &j) {
-            return ratings[i] < ratings[j];
-        });
+        sort(index.begin(), index.end(),
+             [&](auto& i, auto& j) { return ratings[i] < ratings[j]; });
 
-        for(auto &&i : index) {
-            candy[i] = max(i == 0 || ratings[i - 1] == ratings[i] ? 0 : candy[i - 1], i == ratings.size() - 1 || ratings[i + 1] == ratings[i] ? 0 : candy[i + 1]) + 1;
+        for (auto&& i : index) {
+            candy[i] =
+                max(i == 0 || ratings[i - 1] == ratings[i] ? 0 : candy[i - 1],
+                    i == ratings.size() - 1 || ratings[i + 1] == ratings[i]
+                        ? 0
+                        : candy[i + 1]) +
+                1;
         }
 
         return accumulate(candy.begin(), candy.end(), 0);

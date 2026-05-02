67c87b5b86c9a1c057b8119e17cbf3765b8fb77a
garyellow <gary20011110@gmail.com>
2026-05-03 00:50:24 +0800

    Time: 0 ms (100%), Space: 8.1 MB (42.63%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.cpp
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -1,34 +1,44 @@
 class Solution {
+    static vector<int> dp;
+
 public:
-    int rotatedDigits(int n) {
-        vector<int> dp(n + 1, 0);
-        int count = 0;
+    Solution() {
+        if (dp.empty()) {
+            vector<int> temp;
+            dp = temp = vector<int>(1e4 + 1, 0);
 
-        for (int i = 0; i <= n; i++) {
-            if (i < 10) {
-                if (i == 0 || i == 1 || i == 8) {
-                    dp[i] = 1;
-                } else if (i == 2 || i == 5 || i == 6 || i == 9) {
-                    dp[i] = 2;
-                    count++;
-                } else {
-                    dp[i] = 0;
-                }
-            } else {
-                int a = dp[i / 10];
-                int b = dp[i % 10];
+            int count = 0;
 
-                if (a == 1 && b == 1) {
-                    dp[i] = 1;
-                } else if (a >= 1 && b >= 1) {
-                    dp[i] = 2;
-                    count++;
+            for (int i = 0; i <= 1e4; i++) {
+                if (i < 10) {
+                    if (i == 0 || i == 1 || i == 8) {
+                        temp[i] = 1;
+                    } else if (i == 2 || i == 5 || i == 6 || i == 9) {
+                        temp[i] = 2;
+                        count++;
+                    } else {
+                        temp[i] = 0;
+                    }
                 } else {
-                    dp[i] = 0;
+                    int a = temp[i / 10];
+                    int b = temp[i % 10];
+
+                    if (a == 1 && b == 1) {
+                        temp[i] = 1;
+                    } else if (a >= 1 && b >= 1) {
+                        temp[i] = 2;
+                        count++;
+                    } else {
+                        temp[i] = 0;
+                    }
                 }
+
+                dp[i] = count;
             }
         }
-
-        return count;
     }
+
+    int rotatedDigits(int n) { return dp[n]; }
 };
+
+vector<int> Solution::dp;

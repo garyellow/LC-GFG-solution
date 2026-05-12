cd2c029e04998dd0251e4ca17415c56cb0a87fa5
garyellow <gary20011110@gmail.com>
2026-05-12 14:17:58 +0800

    Time: 30 ms (85.92%), Space: 97.4 MB (39.81%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
     int minimumEffort(vector<vector<int>>& tasks) {
-        int max_energy = 0;
-        int sum_energy = 0;
-        int min_diff_energy = numeric_limits<int>::max();
-        for (const auto& task : tasks) {
-            sum_energy += task[0];
-            max_energy = max(max_energy, task[1]);
-            min_diff_energy = min(min_diff_energy, task[1] - task[0]);
-        }
-
         ranges::sort(tasks, [](const auto& x, const auto& y) {
             return x[1] - x[0] > y[1] - y[0];
         });

d08b7ad4546b650c1cba55309d36acf6bc9e5458
garyellow <gary20011110@gmail.com>
2026-06-20 00:09:29 +0800

    Time: 0 ms (100%), Space: 10.9 MB (44.68%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
         int cnt = 0, ans = cnt;
-        
-        for(auto &&g : gain) {
+
+        for (auto g : gain) {
             ans = max(ans, cnt += g);
         }
-        
+
         return ans;
     }
 };

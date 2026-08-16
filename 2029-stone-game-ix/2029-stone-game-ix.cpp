f21198ade6bd60b113aa91cfd6919c43e458604f
garyellow <gary20011110@gmail.com>
2026-08-16 19:53:00 +0800

    Time: 3 ms (75.38%), Space: 131.4 MB (33.5%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    bool stoneGameIX(vector<int> &stones)
+    {
+        vector<int> cnt(3);
+        for (auto &&i : stones)
+            cnt[i % 3]++;
+
+        if (min(cnt[1], cnt[2]) == 0)
+            return max(cnt[1], cnt[2]) > 2 && cnt[0] % 2 > 0;
+        else
+            return abs(cnt[1] - cnt[2]) > 2 || cnt[0] % 2 == 0;
+    }
+};
